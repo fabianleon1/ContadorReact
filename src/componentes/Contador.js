347d7c6d2e88d673d70a12d5b51acb1f476a5731
Terminator <terminator@gmail.com>
2023-06-13 22:07:46 -0600

    Subiendo Contador

---
+++ src/componentes/Contador.js
@@ -0,0 +1,25 @@
+import React from "react";
+import '../hojas-estilos/Contador.css';
+
+
+
+
+function Contador({ numClics }) {
+
+
+    return (
+
+
+        <div className='contador'>
+            {
+                numClics
+            }
+        </div>
+
+
+
+    );
+
+}
+
+export default Contador;
